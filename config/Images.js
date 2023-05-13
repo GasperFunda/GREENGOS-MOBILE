e335991bf5242057951eddf997443a4dbbe58cf8
Gašper Funda Povše <39793309+GasperFunda@users.noreply.github.com>
2023-05-13 12:30:32 +0200

    Initial commit

---
+++ config/Images.js
@@ -0,0 +1,6 @@
+export default {
+  LoginBGImg: require('../assets/images/LoginBGImg.png'),
+  Google: require('../assets/images/Google.png'),
+  Apple: require('../assets/images/Apple.png'),
+  FB: require('../assets/images/FB.png'),
+};
